038e0ede380d2efb596b907b623918e027e22917
abbruno <alainbruno.ss@hotmail.com>
2017-02-20 11:38:19 -0500

    Adding Partial View. Input Not Being Taken

--- FibMVC/Controllers/FiboController.cs
+++ FibMVC/Controllers/FiboController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FibMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,25 @@ namespace FibMVC.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult FiboPage()
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        public PartialViewResult FiboPartial()
+        {
+            //FibonacciMdl fMdl = new FibonacciMdl();
+            return PartialView();
+        }
+
+        [HttpPost]
+        public PartialViewResult FiboPartial(FibonacciMdl fMdl)
+        {
+            return PartialView(fMdl);
         }
     }
 }
---
+++ FibMVC/Models/FibonacciMdl.cs
@@ -0,0 +1,27 @@
+﻿using FibMVC.Biz;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FibMVC.Models
+{
+    public class FibonacciMdl
+    {
+        Fibo fiboObj = new Fibo();
+        public string test;
+
+        [Required]
+        public int lengthOfFibo;
+
+        public FibonacciMdl()
+        {
+        }
+
+        public List<int> GetNumbers(int lngth)
+        {
+            return fiboObj.GetNumbers(lngth);
+        }
+    }
+}
